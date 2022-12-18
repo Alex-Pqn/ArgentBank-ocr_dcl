858b7a41d4292cee5817db8512f365339620b13f
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-18 18:10:45 +0100

    Redux: Auth Actions (login, logout) init

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,40 @@
+import { authService } from '../../_services/auth.service';
+import { STATUS } from '../config/status.js';
+
+const login = (email, password, isRemembered) => {
+  return (dispatch) => {
+    authService
+      .loginOne(
+        { email, password },
+        { 'Content-Type': 'application/json;charset=utf-8' }
+      )
+      .then((retrievedUser) =>
+        dispatch({
+          type: STATUS.LOGIN_SUCCESS,
+          payload: { user: { email, token: retrievedUser.token } },
+        })
+      )
+      .catch((error) =>
+        dispatch({
+          type: STATUS.LOGIN_ERROR,
+          payload: {
+            error,
+          },
+        })
+      );
+  };
+};
+
+const logout = () => {
+  localStorage.clear();
+
+  return (dispatch) =>
+    dispatch({
+      type: STATUS.LOGOUT_REQUEST,
+    });
+};
+
+export const authActions = {
+  login,
+  logout,
+};
