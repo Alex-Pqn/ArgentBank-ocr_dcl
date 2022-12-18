3d56ddf27599c6f2a1d42eb319c7a3f1b765d21f
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-18 18:03:49 +0100

    User Service init

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -2,58 +2,25 @@ import { requestOptions } from '../_helpers/request-options';
 import { userRoutes } from '../config/api/routes/user.routes.config';
 
 export const userService = {
-  signupOne,
-  loginOne,
-  createOneProfile,
+  retrieveOneProfile,
   updateOneProfile,
 };
-
-/**
- * POST: User Signup
- * Signup user
- * @returns User token
- */
-async function signupOne() {
-  return fetch(userRoutes.postUserSignup(), requestOptions.post())
-    .then((res) => {
-      if (!res.ok) return Promise.reject('Unable to signup one user.');
-      return res.json();
-    })
-    .then((data) => data)
-    .catch((err) => {
-      throw err;
-    });
-}
-
-/**
- * POST: User Login
- * Login user
- * @returns User token
- */
-async function loginOne() {
-  return fetch(userRoutes.postUserSignup(), requestOptions.post())
-    .then((res) => {
-      if (!res.ok) return Promise.reject('Unable to login one user.');
-      return res.json();
-    })
-    .then((data) => data)
-    .catch((err) => {
-      throw err;
-    });
-}
 
 /**
  * POST: User Profile
  * Retrieves user profile
  * @returns User Profile
  */
-async function createOneProfile() {
-  return fetch(userRoutes.postUserSignup(), requestOptions.post())
+async function retrieveOneProfile(payload, customHeaders) {
+  return fetch(
+    userRoutes.userProfile(),
+    requestOptions.post(payload, customHeaders)
+  )
     .then((res) => {
-      if (!res.ok) return Promise.reject('Unable to create one user profile.');
+      if (!res.ok) return Promise.reject('Unable to get one user profile.');
       return res.json();
     })
-    .then((data) => data)
+    .then((res) => res.body)
     .catch((err) => {
       throw err;
     });
@@ -62,14 +29,18 @@ async function createOneProfile() {
 /**
  * PUT: User Profile
  * Update user profile
+ * @returns User Profile
  */
-async function updateOneProfile() {
-  return fetch(userRoutes.postUserSignup(), requestOptions.put())
+async function updateOneProfile(payload, customHeaders) {
+  return fetch(
+    userRoutes.userProfile(),
+    requestOptions.put(payload, customHeaders)
+  )
     .then((res) => {
       if (!res.ok) return Promise.reject('Unable to update one user profile.');
       return res.json();
     })
-    .then((data) => data)
+    .then((res) => res.body)
     .catch((err) => {
       throw err;
     });
