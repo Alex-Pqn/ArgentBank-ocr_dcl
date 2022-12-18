fbddae479dab20609e3d43b3ade2aa564b00b37b
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-18 18:57:07 +0100

    Import User model

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -1,6 +1,8 @@
 import { requestOptions } from '../_helpers/request-options';
 import { userRoutes } from '../config/api/routes/user.routes.config';
 
+import UserModel from '../_models/user/user.model';
+
 export const userService = {
   retrieveOneProfile,
   updateOneProfile,
@@ -20,7 +22,7 @@ async function retrieveOneProfile(payload, customHeaders) {
       if (!res.ok) return Promise.reject('Unable to get one user profile.');
       return res.json();
     })
-    .then((res) => res.body)
+    .then((res) => new UserModel(res.body))
     .catch((err) => {
       throw err;
     });
@@ -40,7 +42,7 @@ async function updateOneProfile(payload, customHeaders) {
       if (!res.ok) return Promise.reject('Unable to update one user profile.');
       return res.json();
     })
-    .then((res) => res.body)
+    .then((res) => new UserModel(res.body))
     .catch((err) => {
       throw err;
     });
