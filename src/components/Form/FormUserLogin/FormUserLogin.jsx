554e43dc323621e041d58a190b81b09ea9e23647
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-18 18:09:28 +0100

    FormUserLogin component init

---
+++ src/components/Form/FormUserLogin/FormUserLogin.jsx
@@ -0,0 +1,65 @@
+import './FormUserLogin.scss';
+
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../../../redux/actions/auth.js';
+
+const FormUserLogin = () => {
+  const dispatch = useDispatch();
+
+  const { user, error } = useSelector((state) => state.userReducers);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRemembered, setIsRemembered] = useState('');
+
+  const updateEmailState = (event) => setEmail(event.target.value);
+  const updatePasswordState = (event) => setPassword(event.target.value);
+  const updateIsRememberedState = (event) =>
+    setIsRemembered(event.target.value);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(authActions.login(email, password, isRemembered));
+  };
+
+  if (user.isLogged) return <Navigate to="/profile" />;
+
+  return (
+    <form className="user-login-form" onSubmit={onSubmit}>
+      <div className="input-wrapper">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          onChange={updateEmailState}
+          autoComplete="username"
+        />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={updatePasswordState}
+          autoComplete="current-password"
+        />
+      </div>
+      <div className="input-remember">
+        <input
+          type="checkbox"
+          id="remember-me"
+          onChange={updateIsRememberedState}
+        />
+        <label htmlFor="remember-me">Remember me</label>
+      </div>
+      <div className="error">
+        <span>{error}</span>
+      </div>
+      <button>Sign In</button>
+    </form>
+  );
+};
+
+export default FormUserLogin;
